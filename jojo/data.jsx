125bc5926ad4d36c374e0d0b372d7b5d83b0cfdd
clemzouuu <92720413+clemzouuu@users.noreply.github.com>
2022-08-24 12:41:54 +0200

    up to date, but the audio can play simoulteanously

--- jojo/data.jsx
+++ jojo/data.jsx
@@ -2,26 +2,31 @@ export default [
   {
     name: 'Jonathan Joestar',
     character: './img/jonathan.webp',
+    music:'./music/jonathan.mov'
   },
   {
     name: 'Joseph Joestar',
     character: './img/joseph.webp',
+    music:'./music/joseph.mov'
   },
   {
     name: 'Jotaro Kujo',
     character: './img/jotaro.png',
+    music:'./music/jotaro.mov'
   },
   {
     name: 'Josuke Higashikata',
     character: './img/josuke.webp',
+    music:'./music/josuke.mov'
   },
   {
     name: 'Giorno Giovanna',
     character: './img/giorno.webp',
+    music:'./music/giorno.mov'
   },
-
   {
     name: 'Jolyne Cujoh',
     character: './img/jolyne.webp',
+    music:'./music/jolyne.mov'
   },
 ]
--- jojo/src/App.jsx
+++ jojo/src/App.jsx
@@ -2,32 +2,57 @@ import './App.css'
 import data from '../data'
 import React, {useState, useEffect} from "react"
 
-function App() {
 
+function App() {
+  // By default, the first character is the first data on the data array.
   const [character, setCharacter] = useState(data[0])
+
+  // A counter is created to iterated the array of data.
   const [counter, setCounter] = useState(0)
 
+  // The character displayed on the screen is based on the counter.
+  // The state containing the data will be changed only if the value of counter changes.
+  // Thus, if the value of the counter changes, the data displayed will change.
   useEffect(() => {
     setCharacter(data[counter])
   }, [counter])
 
-  console.log(counter)
-
+  // If an arrow is clicked and counter's value < 5, the counter value will increase of 1.
   function nextCharacter() {
-    if(counter < 5){
-      setCounter(counter + 1) 
+    if (counter < 5) {
+      setCounter(counter + 1)
     }
   }
 
-  function previousCharacter(){
-    if(counter > 0){
-      setCounter(counter - 1) 
+  // If an arrow is clicked and counter's value > 0, the counter value will decrease of 1.
+  function previousCharacter() {
+    if (counter > 0) {
+      setCounter(counter - 1)
     }
   }
+
+  // A counter to avoid the audio to be played several times before it ends.
+  // If the function is called, play ++, and play plays itself only if it equals 1
+  let play = 0
+  
+  function playMusic() {
+
+    play ++
+    if (play == 1) {
+      var audio = new Audio(character.music)
+      audio.play() 
+    }
+
+    // After 3 seconds, you can listen to the audio again
+    setTimeout(() => {
+      play = 0 
+    }, 3000)
+  }
+
 
   return (
     <div className="App">
-      <div className='main'>
+      <div className="main">
         <img src="../img/logo.png" alt="logo JBA" />
         <h1>{character.name}</h1>
 
@@ -50,6 +75,7 @@ function App() {
           className="arrow_right"
           onClick={nextCharacter}
         />
+        <img className="music" onClick={playMusic} src="../svg/sound_on.svg" />
       </div>
     </div>
   )
